8e370526ce33ef21952b1f7a511d45e9a4a95d97
Victor <victor.azarenko@gmail.com>
2022-09-29 21:22:49 +0300

    Example004 Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,5 +1,3 @@
-﻿double numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
-Console.WriteLine(numberA);
-double numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-Console.WriteLine (numberA + numberB);
+﻿double numberA = 10; 
+double numberB = 5;
+Console.WriteLine (numberA / numberB);
